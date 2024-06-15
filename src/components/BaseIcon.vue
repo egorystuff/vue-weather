dedc95f29a7260ac9c62d07ab3fa2545ff4639f2
Yahor Yankavets <mr.yankovec@gmail.com>
2024-06-16 01:09:41 +0300

    update test

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import icons from '../assets/icons/icons'
+import type { IconName } from '../types'
+
+defineProps<{ name: IconName }>()
+</script>
+
+<template>
+  <component :is="icons[name]" :class="$attrs.class || 'h-5'" />
+</template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BaseIcon from './BaseIcon.vue'
+</script>
+
+<template>
+  <BaseIcon name="menu" class="w-5 h-5" />
+  <div>Vue Weather</div>
+  <div>header</div>
+</template>
