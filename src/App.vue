b081b5acfe72f77e731e491c39c995e2c866e7f8
Yahor Yankavets <mr.yankovec@gmail.com>
2024-06-21 15:04:10 +0300

    added axios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { changeTheme, storage, theme } from './changeTheme'
+import axios from 'axios'
 import TheHome from './pages/TheHome.vue'
 import TheHeader from './components/TheHeader.vue'
 import ThePopup from './components/ThePopup.vue'
 
+// const fetchItems = async () => {
+//   try {
+//     const { data } = await axios.get(
+//       `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=9437c51ae0fde293b89674bf3d56b8d2`
+//     )
+//     console.log(data)
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
 onMounted(() => {
+  fetchItems()
   theme.value = storage.getItem('theme')
 })
 </script>
